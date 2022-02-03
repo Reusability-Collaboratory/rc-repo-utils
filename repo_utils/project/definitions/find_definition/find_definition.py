f3f1fa77d8f4bc63011b229e4b837109e9142f7d
Joshua Mathias <joshuaamathias@gmail.com>
2022-02-02 16:42:30 -0800

    Add containing_folder arg to find_definition

--- repo_utils/project/definitions/find_definition/find_definition.py
+++ repo_utils/project/definitions/find_definition/find_definition.py
@@ -4,28 +4,43 @@ from os.path import join
 import git
 
 
-def find_definition(def_name: str, namespace: str = None):
+def find_definition(def_name: str, containing_folder: str=None, namespace: str = None):
     """
     Searches among modules directories and
     returns the directory path that matches def_name
     under the specified namespace directory
-    """
-    repo = git.Repo(os.getcwd(), search_parent_directories=True)
-    src_root = repo.working_tree_dir
-    # src_root = join(repo_root, repo.name) # subfolder with same name
-    level_offset = len(src_root.split(os.sep)) - 1
 
-    for dir_path, dirs, _ in os.walk(src_root):
-        for dir_name in dirs:
-            if dir_name == "build":
+    Args:
+        - containing_folder (str): Search only within this directory path. Current working directory by default.
+        - namespace (str): Within containing_folder, find a folder with this name and search only under this folder.
+    """
+    if containing_folder is None:
+        src_root = os.getcwd()
+    else:
+        repo = git.Repo(containing_folder, search_parent_directories=True)
+        src_root = repo.working_tree_dir
+    # src_root = join(repo_root, repo.name) # subfolder with same name
+    # level_offset = len(src_root.split(os.sep)) - 1
+
+    if namespace is None:
+        for dir_path, dirs, _ in os.walk(src_root):
+            if dir_path == "build" or dir_path.endswith(os.sep + "build"):
                 continue
-            if not namespace or dir_name == namespace:
-                for space_path, space_dirs, space_files in os.walk(
-                    join(dir_path, dir_name)
-                ):
-                    for def_dir in space_dirs:
-                        if def_dir == def_name:
-                            return join(space_path, def_dir)
-                if namespace:
-                    break
+            for dir_name in dirs:
+                if dir_name == def_name:
+                    return join(dir_path, dir_name)
+    else:
+        for dir_path, dirs, _ in os.walk(src_root):
+            for dir_name in dirs:
+                if dir_name == "build":
+                    continue
+                if not namespace or dir_name == namespace:
+                    for space_path, space_dirs, space_files in os.walk(
+                        join(dir_path, dir_name)
+                    ):
+                        for def_dir in space_dirs:
+                            if def_dir == def_name:
+                                return join(space_path, def_dir)
+                    if namespace:
+                        break
     return None
